a8c89f00936587838dc736ec689b8747fce61180
Owenx25 <owentmccormack@gmail.com>
2022-04-28 08:01:09 -0400

    source formatting and some entry styling

--- src/components/CompendiumEntry.js
+++ src/components/CompendiumEntry.js
@@ -6,8 +6,8 @@ export default function CompendiumEntry({entryData}) {
     // compendium information like location, or known locations.
     // Could also just save these for modal!
     return (
-        <div className={styles.box} style={{backgroundColor: `var(${COLORS.background})`}}>
-            <div>
+        <div className={styles.box}>
+            <div style={{height: "100%"}}>
                 <div className={styles['content-container']}>
                     <p>
                         <span className="entry-title-font" style={{color: `var(${COLORS.content})`, marginRight: "5px"}}>{entryData.title}.</span>
@@ -15,23 +15,51 @@ export default function CompendiumEntry({entryData}) {
                     </p>
                 </div>
                 <div className={styles['tags-container']}>
-                    {/* TODO make these tags dynamically generated! */}
-                    <p>
-                        <span className="entry-content-font" style={{color: `var(${COLORS.content})`}}>Author: </span>
-                        <span className="entry-content-font" style={{color: `var(${COLORS.subtext})`}}>{entryData.author}</span>
-                    </p>
+                    {buildTags(entryData)}
                 </div>
             </div>
             <div className={styles['source-container']}>
-                <p className="entry-source-font" style={{color: `var(${COLORS.content})`, margin: 0}}>
-                    Source: {buildSource(entryData)}
-                </p>
+                {buildSource(entryData)}
             </div>
         </div>
     )
 }
 
 function buildSource(entryData) {
-    // Use response data to format source
-    return "fully formatted source"
+    const source = entryData.source;
+    let sourceElement;
+    if (source.hasOwnProperty('parentSource')) {
+        sourceElement = <span>"{source.title}", <i>{source.parentSource}</i> ({source.year}), {source.author}</span>
+    } else {
+        sourceElement = <span><i>{source.title}</i> ({source.year}), {source.author}</span>
+    } 
+
+
+    return (
+        <p className="entry-source-font" style={{color: `var(${COLORS.content})`, margin: 0}}>
+            Source: {sourceElement}
+        </p>
+    )
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function buildTags(entryData) {
+    const tagElements = [];
+
+    for (const [label,value] of Object.entries(entryData.tags)) {
+        tagElements.push(
+            <p style={{margin: 0, lineHeight: "1.3"}} key={label}>
+                <span className="entry-content-font" style={{color: `var(${COLORS.content})`}}>
+                    {capitalize(label) + ': '}
+                </span>
+                <span className="entry-content-font" style={{color: `var(${COLORS.subtext})`}}>
+                    {value}
+                </span>
+            </p>
+        )
+    }
+    return tagElements;
 }
--- src/components/CompendiumPage.js
+++ src/components/CompendiumPage.js
@@ -20,24 +20,44 @@ export default function CompendiumPage(props) {
         {
             "compendium": "texts",
             "title": "20 Simple Steps to Ventriloquism",
-            "source": "The Secret of Ventriloquism",
-            "author": "Joseph Snavely",
             "description": "The text describes the necessary steps to become a ventriloquist of varying ability. The first eight steps are entirely normal while the subsequent steps begin to teach a more sinister and powerful form of ventriloquism. The more powerful form, greater ventriloquism, is one who can control “dummies” which are not inanimate, though the process is much more difficult and requires pulling many more strings. Greater ventriloquists themselves are themselves are only puppets of a mysterious Ultimate Ventriloquist. The text also includes instructions to harm a homeless man, and take down a plane in Dunnstown. The book is also a metatext.",
+            "tags": {
+                "author": "Joseph Snavely",
+                
+            },
+            "source": {
+                "title": "The Secret of Ventriloquism",
+                "year": "2016",
+                "author": "Jon Padgett"
+            },
             "references": ["The Infusorium", "The Secret of Ventriloquism"]
         },
         {
             "compendium": "texts",
             "title": "Atlas of Untrod Paths, The",
-            "source": "Lovecraft Country",
-            "author": "Unknown",
-            "description": "Presumably a text concerning natural philosophy, but certainly an occult tome of some sort which is of particular interest because it was sold by a rogue member of the Braithwhite family, Henry Braithwhite (using alias Henry Narrow) to fund an escape with his black wife, Pearl."
+            "description": "Presumably a text concerning natural philosophy, but certainly an occult tome of some sort which is of particular interest because it was sold by a rogue member of the Braithwhite family, Henry Braithwhite (using alias Henry Narrow) to fund an escape with his black wife, Pearl.",
+            "tags": {
+                "author": "Unknown"
+            },
+            "source": {
+                "title": "Lovecraft Country",
+                "year": "2016",
+                "author": "Matt Ruff"
+            }
         },
         {
             "compendium": "texts",
             "title": "Black As Darkness",
-            "source": "Black As Darkness",
-            "author": "Various, A film",
-            "description": "A black and white 1940’s film starring Verna Karndess. The film was adapted from the Lilith Blake short story “The Reunion” about a ghostly murderess stalking the streets. Oxford Don Muswell warned against the filming, stating that Blake’s work was a form of incantations which would cause unforeseen consequences. The audio and video was distorted and fights broke out at its first viewing because clips were missing and added and distorted. A hissing background static persists throughout the film. Only a small amount of bootleg copies remain. Verna’s widower learned of her murder by watching the film."
+            "description": "A black and white 1940’s film starring Verna Karndess. The film was adapted from the Lilith Blake short story “The Reunion” about a ghostly murderess stalking the streets. Oxford Don Muswell warned against the filming, stating that Blake’s work was a form of incantations which would cause unforeseen consequences. The audio and video was distorted and fights broke out at its first viewing because clips were missing and added and distorted. A hissing background static persists throughout the film. Only a small amount of bootleg copies remain. Verna’s widower learned of her murder by watching the film.",
+            "tags": {
+                "author": "Various, A film",
+            },
+            "source": {
+                "title": "Black As Darkness",
+                "parentSource": "The White Hands and Other Weird Tales",
+                "year": "2006",
+                "author": "Mark Samuels"
+            }
         },
     ]
 
